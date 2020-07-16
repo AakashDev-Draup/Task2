9a1f04a596e665f6ce5078ef4b67be486c230e55
Aakash Dev <aakash.dev@draup.com>
2020-07-16 19:40:18 +0530

    "comments added"

--- Main.py
+++ Main.py
@@ -5,13 +5,18 @@ makeconnection()
 
 
 class User(Document):
-    """This class has all the attributes for a new user"""
+    """:parameter
+    username : username unique to the database
+    """
     username = StringField(unique=True, required=True, max_length=10)
     meta = {'allow_inheritance': True}
 
 
 class accesslevel(User):
-    """This class has the roles and actions for the user from the User class"""
+    """:parameter
+    roles : list of roles assigned to the user
+    actions : list of all the access levels available to the user
+    """
     roles = ListField(StringField())
     actions = ListField(StringField())
 
@@ -29,14 +34,14 @@ if __name__ == "__main__":
         print("Enter 7 to exit")
         val = int(input("Please enter your choice:"))
         if val == 1:
-            from useroperation import deleteuser
+            from UserOperationFunctions import deleteuser
             user = adduser()
             user.save()
         elif val == 2:
-            from useroperation import adduser
+            from UserOperationFunctions import adduser
             deleteuser()
         elif val == 3:
-            from useroperation import updateuser
+            from UserOperationFunctions import updateuser
             updateuser()
         elif val == 4:
             name = input("Please enter the username:")
@@ -44,7 +49,7 @@ if __name__ == "__main__":
             if obj:
                 enter = input("Enter r to Read, w to write and d to delete:")
                 if enter in obj.actions:
-                    from useroperation import accessdata
+                    from UserOperationFunctions import accessdata
                     accessdata(enter)
                 else:
                     print("You don't have the required access level")
@@ -55,7 +60,7 @@ if __name__ == "__main__":
             name = input("Please enter the username:")
             obj = User.objects(username=name).get()
             if obj:
-                from userroles import addrole
+                from UserRoleFunctions import addrole
                 addrole(obj)
             else:
                 print("user not found")
@@ -63,7 +68,7 @@ if __name__ == "__main__":
             name = input("Please enter the username:")
             obj = User.objects(username=name).get()
             if obj:
-                from userroles import removerole
+                from UserRoleFunctions import removerole
                 removerole(obj)
             else:
                 print("user not found")
--- README.md
+++ README.md
@@ -1,6 +1,39 @@
 # Task2
 
-Module 1 is the main file
+Hello,
+
+To access the Role based control app run the main.py using python interpreter.
+It gives the following options:
+
+Welcome to xyz:
+
+Enter 1 to Add user: To add a new user you select this option. You have to enter a username that is unique to the database.
+
+Enter 2 to Delete user: To delete a user from the database you use this option.
+  
+Enter 3 to Update user details: To update a user record from the database you use this option.
+
+Enter 4 to access data: To access the resources in database you have to use this option. 
+You need to enter your username and the required operation you wantto perform i.e. read , write or delete. 
+You should have the appropriate access level to perform the operation.
+
+Enter 5 to assign role: You can assign the roles for a user using this option. You need to enter the username for the user here.
+A single user can have multiple roles and based on these roles the access levels are assigned to the user.
+
+Enter 6 to remove role: You can remove the roles from a given user using this option.You need to enter the username for the user here.
+You can remove multiple roles of a user from the database. This also modifies the access levels based on role removal.
+
+Enter 7 to exit
+
+
+Following points are to be Noted:
+
+The role based access control was developed on a local server.
+
+Add user, delete user , update user and access resource from the database were imported from pre created functions.
+
+
+Following is the problem statement:
 
 Role Based Access Control:
 
@@ -19,6 +52,3 @@ Note:
 • You do not need to create functionality to add/delete/update users or any other entities. You can get it pre-created.
 
 
-Notes:
-The role based access control was developed on a local server.
-Add user, delete user , update user and access resource imported from pre created functions.
