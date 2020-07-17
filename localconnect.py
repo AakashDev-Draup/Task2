57ea821fb25dd535f0fc75e56c2c53458bd3381c
Aakash Dev <aakash.dev@draup.com>
2020-07-17 12:14:31 +0530

    "Program edited to make more comprehensive"

--- localconnect.py
+++ localconnect.py
@@ -1,4 +1,6 @@
+from mongoengine import connect
+
+
 def makeconnection():
     """This function is used to make connection to the local database"""
-    from mongoengine import connect
     connect("LocalDatabase")
